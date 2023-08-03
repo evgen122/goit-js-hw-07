96a59f9a6fce28dedfd726b92162a012677ef117
evgen122 <evg.khristenko@gmail.com>
2023-08-03 14:42:42 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,7 +3,7 @@
 import {galleryItems} from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
 
 const galleryObj = document.querySelector(".gallery");
 
@@ -12,17 +12,35 @@ function galleryElements(arr) {
     .map(
       ({preview, original, description}) =>
         `<li class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-  alt="${description}"
-    />
-  </a>
-</li>`
+            <a class="gallery__link" href="${original}">
+              <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+              />
+            </a>
+         </li>`
     )
     .join("");
 }
 
 galleryObj.insertAdjacentHTML("beforeend", galleryElements(galleryItems));
+
+galleryObj.addEventListener("click", handleClickOpen);
+
+function handleClickOpen(event) {
+  // console.log(event);
+  const element = event.target.closest(".gallery__image");
+  console.log(element.dataset);
+  const {source} = element.dataset;
+
+  galleryItems.find(({original}) => source === original);
+  const instance = basicLightbox.create(`
+      <img
+      class="gallery__image"
+      src="${source}"       
+    />
+  `);
+  instance.show();
+}
