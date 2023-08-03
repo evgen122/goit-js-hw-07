e0cf8cf3d790d47fb55b8b93f63d7458401b0c82
evgen122 <evg.khristenko@gmail.com>
2023-08-03 16:05:20 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -30,17 +30,31 @@ galleryObj.insertAdjacentHTML("beforeend", galleryElements(galleryItems));
 galleryObj.addEventListener("click", handleClickOpen);
 
 function handleClickOpen(event) {
+  if (event.target === event.currentTarget) {
+    return;
+  }
+
+  event.preventDefault();
+
   // console.log(event);
   const element = event.target.closest(".gallery__image");
-  console.log(element.dataset);
+  // console.log(element.dataset);
   const {source} = element.dataset;
 
-  galleryItems.find(({original}) => source === original);
+  const pictGallery = galleryItems.find(({original}) => source === original);
+  // console.log(pictGallery);
   const instance = basicLightbox.create(`
       <img
       class="gallery__image"
-      src="${source}"       
+      src="${source}" 
+      alt="${pictGallery.description}"      
     />
   `);
   instance.show();
+
+  $basicLightbox__background: rgba(0, 0, 0, 0.8); // Background color
+  $basicLightbox__zIndex: 1000; // Stack order
+  $basicLightbox__duration: 0.4; // Transition duration
+  $basicLightbox__timing: ease; // Transition timing
+  // import "src/styles/main";
 }
